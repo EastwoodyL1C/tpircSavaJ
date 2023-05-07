8c6bf7e9c00bd4d3ad8b465bfdb641756265d133
EastwoodyL1C <alex_eastwoody@yahoo.com>
2023-05-07 03:12:22 +0200

    Update img-pack.js

--- img-pack.js
+++ img-pack.js
@@ -1,19 +1,19 @@
 const img1 = document.createElement('img')
 img1.src = './img/1.jpg'
-img1.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;'
+img1.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;padding: 5px'
 
 const img2 = document.createElement('img')
 img2.src = './img/2.jpg'
-img2.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;'
+img2.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;padding: 5px'
 
 const img3 = document.createElement('img')
 img3.src = './img/3.jpg'
-img3.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: center;'
+img3.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: center;padding: 5px'
 
 const img4 = document.createElement('img')
 img4.src = './img/4.jpg'
-img4.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;'
+img4.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;padding: 5px'
 
 const img5 = document.createElement('img')
 img5.src = './img/5.jpg'
-img5.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;'
+img5.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;padding: 5px'
