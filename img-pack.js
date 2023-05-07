f7fdecb105f8ad4ccb3272bbf618bea9a4265686
EastwoodyL1C <alex_eastwoody@yahoo.com>
2023-05-07 03:01:16 +0200

    upd

---
+++ img-pack.js
@@ -0,0 +1,19 @@
+const img1 = document.createElement('img')
+img1.src = './img/1.jpg'
+img1.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;'
+
+const img2 = document.createElement('img')
+img2.src = './img/2.jpg'
+img2.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;'
+
+const img3 = document.createElement('img')
+img3.src = './img/3.jpg'
+img3.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: center;'
+
+const img4 = document.createElement('img')
+img4.src = './img/4.jpg'
+img4.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;'
+
+const img5 = document.createElement('img')
+img5.src = './img/5.jpg'
+img5.style.cssText = 'width: 200px; height:270px;object-fit:cover; object-position: right;'
--- script.js
+++ script.js
@@ -1,16 +1,28 @@
+const body = document.querySelector('body')
 const wrapper = document.createElement('wrapper')
+body.prepend(wrapper)
 
-//header set
+////////////////////header set
 const header = document.createElement('header')
 wrapper.prepend(header)
 
-//main set
+
+////////////////////main set
 const main = document.createElement('main')
 wrapper.prepend(main)
+
+//start button
 const start = document.createElement('button')
-main.prepend(start)
 start.innerHTML = 'Start'
 
-//footer set
+//minigame img
+const imgPack = document.createElement('div')
+main.prepend(imgPack)
+imgPack.prepend(img3)
+imgPack.prepend(img2)
+imgPack.prepend(img1)
+main.prepend(start)
+
+////////////////////footer set
 const footer = document.createElement('footer')
 wrapper.prepend(footer)
