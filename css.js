4d4a085dc67c2a3c6777777d621494c55db72107
EastwoodyL1C <alex_eastwoody@yahoo.com>
2023-05-11 00:05:03 +0200

    Update css.js

--- css.js
+++ css.js
@@ -2,6 +2,6 @@ body.style.cssText = 'margin: 0'
 wrapper.style.cssText = 'padding: 20px 10px;margin: 0'
 //main
 main.style.cssText = 'display:flex; align-items: center; justify-content: center; padding: 20px 0; flex-direction: column'
-start.style.cssText = 'max-width: 8%; padding: 7px 30px; background-color: transparent; border-radius: 10%;display:flex;justify-content:center;align-items:center;'
-restart.style.cssText = 'max-width: 8%; padding: 7px 30px; background-color: transparent; border-radius: 10%;'
+start.style.cssText = 'max-width: 8%; padding: 7px 30px; background-color: transparent; border-radius: 10%; cursor: pointer; display: flex; align-items: center; justify-content: center'
+restart.style.cssText = 'max-width: 8%; padding: 7px 30px; background-color: transparent; border-radius: 10%; cursor: pointer; display: flex; align-items: center; justify-content: center'
 imgPack.style.cssText = 'padding: 100px 0px 20px 0px; width: 80%;'
