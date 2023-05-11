005b6ee0c40efc522793e1b2f9921e582b3334cd
EastwoodyL1C <alex_eastwoody@yahoo.com>
2023-05-11 20:41:31 +0200

    beta v0.1
    +styles
    +bugs

--- css.js
+++ css.js
@@ -1,7 +1,8 @@
 body.style.cssText = 'margin: 0'
-wrapper.style.cssText = 'padding: 20px 10px;margin: 0'
+wrapper.style.cssText = 'padding: 0px 10px;margin: 0'
 //main
 main.style.cssText = 'display:flex; align-items: center; justify-content: center; padding: 20px 0; flex-direction: column'
-start.style.cssText = 'max-width: 8%; padding: 7px 30px; background-color: transparent; border-radius: 10%; cursor: pointer; display: flex; align-items: center; justify-content: center'
-restart.style.cssText = 'max-width: 8%; padding: 7px 30px; background-color: transparent; border-radius: 10%; cursor: pointer; display: flex; align-items: center; justify-content: center'
-imgPack.style.cssText = 'padding: 40px 0px 20px 0px; width: 80%;'
+start.style.cssText = 'max-width: 8%; padding: 7px 30px; background-color: transparent; border-radius: 10%; cursor: pointer; display: flex; align-items: center; justify-content: center;font-family: "Open Sans", sans-serif; font-size: 16px; letter-spacing: 2px;text-decoration: none;text-transform: uppercase;color: #000;border: 3px solid;box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px;margin:0'
+imgPack.style.cssText = 'padding: 40px 0px 20px 0px; width: 80%; display: flex; justify-content: center; flex-direction: column'
+imgRow1.style.cssText = 'display: flex; flex-direction: row; justify-content: center'
+imgRow2.style.cssText = 'display: flex; flex-direction: row; justify-content: center'
--- script.js
+++ script.js
@@ -15,30 +15,24 @@ const footer = document.createElement('footer')
 const start = document.createElement('button')
 start.innerHTML = 'Start'
 
-//restart button
-const restart = document.createElement('button')
-restart.innerHTML = 'Restart'
-
 //flip function
 start.onclick = function () {
-    imgRow1.style.cssText = 'display: flex; flex-direction: row'
-    imgRow2.style.cssText = 'display: flex; flex-direction: row'
-    arrBack.forEach(el => el.style = 'width: 18%; object-fit:cover; object-position: right; padding: 5px; border-radius: 10%; display: block;')
-    arrFront.forEach(el => el.style = 'width: 18%; object-fit:cover; object-position: right; padding: 5px; border-radius: 10%; display: block;')
+    arrBack.forEach(el => el.style = 'width: 18.5%; object-fit:cover; object-position: right; padding: 5px; border-radius: 10%; display: block;')
+    arrFront.forEach(el => el.style = 'width: 18.5%; object-fit:cover; object-position: right; padding: 5px; border-radius: 10%; display: block;')
     setTimeout(function() {
-        arrBack.forEach(el => el.style = 'width: 18%; object-fit:cover; object-position: right; padding: 5px; border-radius: 10%; filter: brightness(0);')
-        arrFront.forEach(el => el.style = 'width: 18%; object-fit:cover; object-position: right; padding: 5px; border-radius: 10%; filter: brightness(0);')
+        arrBack.forEach(el => el.style = 'width: 18.5%; object-fit:cover; object-position: right; padding: 5px; border-radius: 10%; filter: brightness(0); transition:0.4s all ease-in-out')
+        arrFront.forEach(el => el.style = 'width: 18.5%; object-fit:cover; object-position: right; padding: 5px; border-radius: 10%; filter: brightness(0); transition:0.4s all ease-in-out')
     },7500)
-}
-//reset minigame
-restart.onclick = function () {
-    window.location.reload()
+
+    //reset minigame
+    start.onclick = function () {
+        window.location.reload()
+    }
 }
 
 body.prepend(wrapper)
 wrapper.prepend(footer)
 wrapper.prepend(main)
-main.prepend(restart)
 
 //minigame img
 const imgPack = document.createElement('div')
