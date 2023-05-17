47913f96af0cf36bdd921552f616394bceb0c934
EastwoodyL1C <alex_eastwoody@yahoo.com>
2023-05-18 00:42:51 +0200

    fix

--- script.js
+++ script.js
@@ -22,46 +22,39 @@ start.onclick = function () {
         arrBack.forEach(el => el.style = imgStyleNotVisible)
         arrFront.forEach(el => el.style = imgStyleNotVisible)
         //choose and check
-        //timeout
         const state = {
             valueFront: [],
             valueBack: []
         }
-        const mainCheck = function () {
-            //[check-front] =>
-            const checkFront = document.querySelectorAll('.main__minigame-row1')
-            checkFront.forEach( elFront => elFront.addEventListener('click', () => {
-                for (let i = 0; i < 1; i++) {
-                    for (let el in state.valueFront) delete state.valueFront[el]
-                    state.valueFront.push(elFront.getAttribute('image'))
-                    elFront.style = imgStyleSelected
-                }
-            }))
-            //[check-back] =>
-            const checkBack = document.querySelectorAll('.main__minigame-row2')
-            checkBack.forEach( elBack => elBack.addEventListener('click', () => {
-                for (let i = 0; i < 1; i++) {
-                    for (let el in state.valueBack) delete state.valueBack[el]
-                    state.valueBack.push(elBack.getAttribute('image'))
-                    elBack.style = imgStyleSelected
-                }
-            }))
-        }
-        
-
-        //функцию)
+        //[check-front] =>
+        const checkFront = document.querySelectorAll('.main__minigame-row1')
+        checkFront.forEach( elFront => elFront.addEventListener('click', () => {
+            for (let i = 0; i < 1; i++) {
+                for (let el in state.valueFront) delete state.valueFront[el]
+                state.valueFront.push(elFront.getAttribute('image'))
+                elFront.style = imgStyleSelected
+            }
+        }))
+        //[check-back] =>
+        const checkBack = document.querySelectorAll('.main__minigame-row2')
+        checkBack.forEach( elBack => elBack.addEventListener('click', () => {
+            for (let i = 0; i < 1; i++) {
+                for (let el in state.valueBack) delete state.valueBack[el]
+                state.valueBack.push(elBack.getAttribute('image'))
+                elBack.style = imgStyleSelected
+            }
+        }))
         console.log(state)
         for (let k in state) {
             if (state.valueFront[k] == state.valueBack[k]) {
-                state.valueFront[k] = imgStylePassed
-                state.valueBack[k] = imgStylePassed
+                state.valueFront.style = imgStylePassed
+                state.valueBack.style = imgStylePassed
                 console.log('true')
             }else {
                 console.log('false');
             }
         }
     },5000)
-
     //reset minigame
     start.onclick = function () {
         window.location.reload()
